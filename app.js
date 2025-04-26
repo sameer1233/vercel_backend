97b18616e94f4c2b5ad08a4388f611e9b1cea321
Sameer Singh <Sameer0Singh123@gmail.com>
2025-04-27 00:12:08 +0530

    app.js file updated

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(session({
   }));
   
 app.use(cors({
-    origin: ['http://127.0.0.1:5500', 'http://127.0.0.1:5501'],
+    origin: ['http://127.0.0.1:5500', 'http://127.0.0.1:5501', 'https://sample-vercel-frontend.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],       
     credentials: true  
   }));
